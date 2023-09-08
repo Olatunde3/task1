6caa95aea580c6ca8683fbafc69c9e9580196876
Olatunde3 <144429762+Olatunde3@users.noreply.github.com>
2023-09-08 12:01:33 +0300

    Update views.py

--- tasks1/views.py
+++ tasks1/views.py
@@ -1,11 +1,10 @@
-from  rest_framework.response import Response
-from rest_framework.decorators import api_view
+from django.http import JsonResponse
 from .models import Slack
-from .serializer import Slackserializer
+
 # Create your views here.
 
-@api_view(['GET'])
-def getData(request):
-    slack = Slack.objects.all()
-    serializer = Slackserializer(slack, many=True)
-    return Response(serializer.data)
+def my_api(request):
+    data = {
+        'data': list(Slack.objects.values()),
+    }
+    return JsonResponse(data)
